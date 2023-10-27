f3b7aaa2f9921ea43ac616428e1c4bad5497896d
aktk-program <hojiro7760@gmail.com>
2023-10-28 03:42:56 +0900

    edit home.html

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var aElement = document.getElementById("a");
+aElement.style.display = "none"; // #aの要素を非表示にする
+var aElement = document.getElementById("c");
+aElement.style.display = "none"; // #aの要素を非表示にする
+var flag = 1;
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("leftbutton").addEventListener("click", function () {
+    // a以外の全ての要素を非表示にする
+    if (flag > 0 && flag <= 2) {
+      flag -= 1;
+    }
+    var elements = document.getElementsByClassName("element");
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].id !== elements[flag].id) {
+        elements[i].style.display = "none";
+      } else {
+        elements[i].style.display = "grid";
+        elements[i].style.opacity = "0";
+        fadeIn(elements[i]); // フェードイン関数を呼び出す
+      }
+    }
+    console.log(flag);
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("rightbutton").addEventListener("click", function () {
+    // a以外の全ての要素を非表示にする
+    if (flag >= 0 && flag < 2) {
+      flag += 1;
+    }
+    var elements = document.getElementsByClassName("element");
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].id !== elements[flag].id) {
+        elements[i].style.display = "none";
+      } else {
+        elements[i].style.display = "grid";
+        elements[i].style.opacity = "0";
+        fadeIn(elements[i]); // フェードイン関数を呼び出す
+      }
+    }
+    console.log(flag);
+  });
+});
+function fadeIn(element) {
+  var opacity = 0;
+  var timer = setInterval(function () {
+    if (opacity >= 1) {
+      clearInterval(timer);
+    }
+    element.style.opacity = opacity;
+    opacity += 0.1; // フェードインの速さを調整できます
+  }, 50); // フェードインの速さを調整できます
+}
